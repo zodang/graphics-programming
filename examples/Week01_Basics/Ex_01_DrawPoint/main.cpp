c4a894d290ec9cb579e410b343fc8e1cbc1e0a71
zodang <1141ekdms@naver.com>
2026-01-06 00:25:21 +0900

    Week01 예제 파일 추가

---
+++ examples/Week01_Basics/Ex_01_DrawPoint/main.cpp
@@ -0,0 +1,81 @@
+#include <sb7.h>
+
+class my_application : public sb7::application {
+public:
+	GLuint compile_shader()
+	{
+		GLuint vertex_shader;
+		GLuint fragment_shader;
+
+		vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+		fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		const GLchar* vertex_shader_source[] =
+		{
+			"#version 430 core \n"
+			"void main(void) \n"
+			"{ \n"
+			"gl_Position = vec4(0.0f, 0.0f, 0.5f, 1.0f); \n"
+			"} \n"
+		};
+
+		const GLchar* fragment_shader_source[] =
+		{
+			"#version 430 core \n"
+			"out vec4 color; \n"
+			"void main(void) \n"
+			"{ \n"
+			"color = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
+			"} \n"
+		};
+
+		glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
+		glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
+
+		glCompileShader(vertex_shader);
+		glCompileShader(fragment_shader);
+
+		GLuint program;
+		program = glCreateProgram();
+
+		glAttachShader(program, vertex_shader);
+		glAttachShader(program, fragment_shader);
+
+		glLinkProgram(program);
+
+		glDeleteShader(vertex_shader);
+		glDeleteShader(fragment_shader);
+
+		return program;
+	}
+
+	virtual void startup()
+	{
+		rendering_program = compile_shader();
+
+		glGenVertexArrays(1, &vertex_array_object);
+		glBindVertexArray(vertex_array_object);
+	}
+
+	virtual void shutdown()
+	{
+		glDeleteVertexArrays(1, &vertex_array_object);
+		glDeleteProgram(rendering_program);
+	}
+
+	virtual void render(double currentTime)
+	{
+		const GLfloat backgroundColor[] = { 0.2f, 0.3f, 0.3f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, backgroundColor);
+
+		glUseProgram(rendering_program);
+		glPointSize(10.0f);
+		glDrawArrays(GL_POINTS, 0, 1);
+	}
+
+private:
+	GLuint rendering_program;
+	GLuint vertex_array_object;
+};
+
+DECLARE_MAIN(my_application)
